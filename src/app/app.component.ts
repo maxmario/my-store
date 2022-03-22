38b6904aaa080b71c55edc29d0faf85e61d2ec9c
Mario Guerrero <mario.guerrero.contreras@gmail.com>
2022-03-22 00:36:54 -0600

    objetos y eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'Mario';
   age = 32;
-  img = 'https://static.platzi.com/media/meta_tags/og/OG-platzi-Dic-2019.png';
+  img = 'https://cdn.dribbble.com/users/81809/screenshots/3443452/media/6a11de9a8c37373073f10d24cb17aabc.jpg';
+  btnDisabled = true;
+  person = {
+    name: 'Mario',
+    age: 32,
+    avatar: 'https://cdn.dribbble.com/users/81809/screenshots/3443452/media/6a11de9a8c37373073f10d24cb17aabc.jpg'
+  }
+
+  public toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  public incraseAge() {
+    this.person.age += 1;
+  }
 }
